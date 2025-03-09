f50c6edc21507ba318306da923301b96ccd689f3
Stephen O'Mahony <sdomah5@gmail.com>
2025-03-09 16:04:37 +0000

    Cleaning up redundant files

--- frontend/src/Friends.js
+++
@@ -1,7 +0,0 @@
-import { sendFriendRequest } from "../firebase/friendFunctions";
-
-const handleSendRequest = async () => {
-    const userEmail = "user1@gmail.com"; // Get from logged-in user
-    const friendEmail = "user2@gmail.com"; // Get from input field
-    await sendFriendRequest(userEmail, friendEmail);
-};
